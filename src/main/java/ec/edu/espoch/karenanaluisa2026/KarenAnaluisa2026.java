49a63877bc191cc2eebfed55e7ba275429bd7ffc
kar-gl <karenanaluisa40@gmail.com>
2024-11-07 13:35:34 -0500

    practica

---
+++ src/main/java/ec/edu/espoch/karenanaluisa2026/KarenAnaluisa2026.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.karenanaluisa2026;
+
+/**
+ *
+ * @author SO-LAB1-PC14
+ */
+import java.util.Scanner;
+public class KarenAnaluisa2026 {
+
+    public static void main(String[] args) {
+      Scanner arreg 
+        var arreglo
+        
+    }
+}
